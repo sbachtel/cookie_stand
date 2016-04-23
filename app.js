1b894de36e6bc0f9f20b3f649a10b4d1b0eaf8b2
Steve Bachtel <sbachtel@gmail.com>
2016-04-23 16:21:52 -0700

    working on app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-
+var pikePlace = {
+  minCustomer: 17,
+  maxCustomer: 88,
+  avgCookieSale: 5.2,
+  hours: ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"],
+  hourlyCookieSales: [],
+  randNumber: function(maxCustomer, minCustomer){
+    return Math.floor(Math.random()*(this.maxCustomer - this.minCustomer) + this.minCustomer);
+  },
+  getHourlyCookies: function() {
+    for(var i = 0; this.hours.length; i++){
+      var cookiesAnHour = this.randNumber(this.maxCustomer, this.minCustomer) * this.avgCookieSale;
+      this.hourlyCookieSales.push(cookiesAnHour);
+    }
+  }
+}
