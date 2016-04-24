74aa38356dde9d522c003d3efcf6b41e86cf23bb
Steve Bachtel <sbachtel@gmail.com>
2016-04-24 14:35:00 -0700

    working on this damn thing

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
+var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 var pikePlace = {
   minCustomer: 17,
   maxCustomer: 88,
   avgCookieSale: 5.2,
-  hours: ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"],
   hourlyCookieSales: [],
-  randNumber: function(maxCustomer, minCustomer){
-    return Math.floor(Math.random()*(this.maxCustomer - this.minCustomer) + this.minCustomer);
+  totalCookieSales:[] ,
+  randNumber: function(maxCustomer, minCustomer) {
+    return Math.floor(Math.random() * (maxCustomer - minCustomer) + minCustomer);
   },
-  getHourlyCookies: function() {
-    for(var i = 0; this.hours.length; i++){
-      var cookiesAnHour = this.randNumber(this.maxCustomer, this.minCustomer) * this.avgCookieSale;
-      this.hourlyCookieSales.push(cookiesAnHour);
+  getHourlyCookieSales: function() {
+    this.hourlyCookieSales=[];
+    for (var i = 0; i < hours.length; i++){
+    this.hourlyCookieSales.push(this.randNumber(this.maxCustomer, this.minCustomer) * this.avgCookieSale);
+    }
+  },
+  getSum: function(){
+    this.totalCookieSales=[];
+    for (var i = 0; i < hours.length; i++){
+      this.totalCookieSales.push(this.hourlyCookieSales.reduce(totalCookieSales));
     }
   }
-}
+};
