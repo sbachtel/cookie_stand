e176536e9eb1ccdaf26102a2d4bda827365f850f
Steve Bachtel <sbachtel@gmail.com>
2016-04-25 21:06:36 -0700

    worked on pikePlace object

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var hours = ["10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var hours = ["10am: ", "11am: ", "12pm: ", "1pm: ", "2pm: ", "3pm: ", "4pm: ", "5pm: "];
 var pikePlace = {
   name: "Pike Place",
   minCustomer: 17,
@@ -6,17 +6,33 @@ var pikePlace = {
   avgCookieSale: 5.2,
   hourlyCookieSales: [],
   totalCookieSales: 0 ,
-  randNumber: function(maxCustomer, minCustomer) {
-    return Math.floor(Math.random() * (maxCustomer - minCustomer) + minCustomer);
+  randNumber: function(min, max) {
+    return Math.random() * (max - min) + min;
   },
-  getHourlyCookieSales: function() {
-    for (var i = 0; i < hours.length; i++){
-      var cookies = this.randNumber(this.maxCustomer, this.minCustomer) * this.avgCookieSale;
-      this.hourlyCookieSales.push(cookies);
-      this.totalCookieSales += cookies;
+  getHourlyCookieSales: function(arr, totals, avg, rand, hour) {
+    for (var i = 0; i < hour.length; i++){
+      var cookies = Math.floor(this.randNumber(this.minCustomer, this.maxCustomer) * avg);
+      arr.push(cookies);
+      totals += cookies;
     }
+  },
+  render: function(){
+    this.getHourlyCookieSales(this.hourlyCookieSales, this.totalCookieSales, this.avgCookieSale, this.randNumber, hours);
+    var sectionEl = document.getElementById('pikeList');
+    var ulEl = document.createElement('ul');
+    var liEl = document.createElement('li');
+    sectionEl.textContent = this.name;
+
+    for (var i = 0; i < hours.length; i++){
+      liEl.textContent = hours[i] + this.hourlyCookieSales[i];
+      ulEl.appendChild(liEl);
+    }
+    var liList = document.createElement('li');
+    liList.textContent = "grand total is: " + this.totalCookieSales;
+    ulEl.appendChild(liList);
+    sectionEl.appendChild(ulEl);
   }
-}
+};
 
 var seaTac = {
   name: "SeaTac Airport",
@@ -94,3 +110,10 @@ var seaTac = {
           }
         }
       }
+    //  pikePlace.render();
+var stores = [
+  pikePlace,
+];
+for (var k = 0; k < stores.length; k++) {
+  stores[k].render();
+}
